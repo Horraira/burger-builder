93e5b8e06f3c27fe6941357664def37d5fbb71c5
AbuSohan <sohan@inflexionpointbd.com>
2022-09-05 09:43:13 +0600

    Showing Price on Card

--- src/Components/BurgerBuilder/BurgerBuilder.js
+++ src/Components/BurgerBuilder/BurgerBuilder.js
@@ -2,6 +2,12 @@ import React, { Component } from "react";
 import Burger from "./Burger/Burger";
 import Controls from "./Controls/Controls";
 
+const INGREDIENT_PRICES = {
+  salad: 20,
+  cheese: 40,
+  meat: 90,
+};
+
 export default class BurgerBuilder extends Component {
   state = {
     ingredients: [
@@ -9,25 +15,28 @@ export default class BurgerBuilder extends Component {
       { type: "salad", amount: 0 },
       { type: "cheese", amount: 0 },
     ],
+    totalPrice: 80,
   };
 
   addIngredient = (type) => {
     const ingredients = [...this.state.ingredients];
+    const newPrice = this.state.totalPrice + INGREDIENT_PRICES[type];
     for (let item of ingredients) {
       if (item.type === type) item.amount++;
     }
-    this.setState({ ingredients: ingredients });
+    this.setState({ ingredients: ingredients, totalPrice: newPrice });
   };
 
   removeIngredient = (type) => {
     const ingredients = [...this.state.ingredients];
+    const newPrice = this.state.totalPrice - INGREDIENT_PRICES[type];
     for (let item of ingredients) {
       if (item.type === type) {
         if (item.amount <= 0) return;
         item.amount--;
       }
     }
-    this.setState({ ingredients: ingredients });
+    this.setState({ ingredients: ingredients, totalPrice: newPrice });
   };
 
   render() {
@@ -37,6 +46,7 @@ export default class BurgerBuilder extends Component {
         <Controls
           ingredientAdded={this.addIngredient}
           ingredientremoved={this.removeIngredient}
+          price={this.state.totalPrice}
         />
       </div>
     );
--- src/Components/BurgerBuilder/Controls/Controls.js
+++ src/Components/BurgerBuilder/Controls/Controls.js
@@ -53,7 +53,9 @@ const Controls = (props) => {
           })}
         </CardBody>
         <CardFooter>
-          <h5>Price: BDT</h5>
+          <h5>
+            Price: <strong>{props.price}</strong> BDT
+          </h5>
         </CardFooter>
       </Card>
     </div>

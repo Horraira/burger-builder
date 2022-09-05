909967920db3824f4abfa9c03d7e7037f715f1ff
AbuSohan <sohan@inflexionpointbd.com>
2022-09-05 23:04:10 +0600

    Adding Redux

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import { BrowserRouter } from "react-router-dom";
 import Main from "./Components/Main";
 
+import { Provider } from "react-redux";
+import { store } from "./redux/store";
+
 function App() {
   return (
     <div className="App">
-      <BrowserRouter>
-        <Main />
-      </BrowserRouter>
+      <Provider store={store}>
+        <BrowserRouter>
+          <Main />
+        </BrowserRouter>
+      </Provider>
     </div>
   );
 }

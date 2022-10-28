c72e59d74b05f661deb79de32566bceff5c12794
AbuSohan <sohan@inflexionpointbd.com>
2022-10-28 10:12:42 +0600

    Improving Checkout Component

--- src/Components/Orders/Checkout/Checkout.js
+++ src/Components/Orders/Checkout/Checkout.js
@@ -1,6 +1,9 @@
 import React, { Component } from "react";
-import { Button } from "reactstrap";
+import { Button, Modal, ModalBody } from "reactstrap";
+import Spiner from "../../Spiner/Spiner";
+
 import { connect } from "react-redux";
+import { reserIngredients } from "../../../redux/actionCreators";
 import axios from "axios";
 
 const mapStateToProps = (state) => {
@@ -11,6 +14,12 @@ const mapStateToProps = (state) => {
   };
 };
 
+const mapDispatchToProps = (dispatch) => {
+  return {
+    reserIngredients: () => dispatch(reserIngredients()),
+  };
+};
+
 class Checkout extends Component {
   state = {
     values: {
@@ -18,6 +27,9 @@ class Checkout extends Component {
       phone: "",
       paymentType: "Cash On Delivery",
     },
+    isLoading: false,
+    isModalOpen: false,
+    modalMsg: "",
   };
 
   goBack = () => {
@@ -34,6 +46,7 @@ class Checkout extends Component {
   };
 
   submitHandler = () => {
+    this.setState({ isLoading: true });
     const order = {
       ingredients: this.props.ingredients,
       customer: this.state.values,
@@ -45,12 +58,33 @@ class Checkout extends Component {
         "https://burgerbuilder-5f84b-default-rtdb.asia-southeast1.firebasedatabase.app/orders.json",
         order
       )
-      .then((response) => console.log(response))
-      .catch((err) => console.log(err));
+      .then((response) => {
+        if (response.status == 200) {
+          this.setState({
+            isLoading: false,
+            isModalOpen: true,
+            modalMsg: "Order placed Successfully",
+          });
+          this.props.reserIngredients();
+        } else {
+          this.setState({
+            isLoading: false,
+            isModalOpen: true,
+            modalMsg: "Something went wrong!",
+          });
+        }
+      })
+      .catch((err) => {
+        this.setState({
+          isLoading: false,
+          isModalOpen: true,
+          modalMsg: "Something went wrong!",
+        });
+      });
   };
 
   render() {
-    return (
+    let Form = (
       <div>
         <h4
           style={{
@@ -99,6 +133,7 @@ class Checkout extends Component {
           <Button
             style={{ backgroundColor: "#D70F64", marginRight: "4px" }}
             onClick={this.submitHandler}
+            disabled={!this.props.purchasable}
           >
             Place Order
           </Button>
@@ -108,7 +143,17 @@ class Checkout extends Component {
         </form>
       </div>
     );
+    return (
+      <div>
+        {this.state.isLoading ? <Spiner /> : Form}
+        <Modal isOpen={this.state.isModalOpen} onClick={this.goBack}>
+          <ModalBody>
+            <p>{this.state.modalMsg}</p>
+          </ModalBody>
+        </Modal>
+      </div>
+    );
   }
 }
 
-export default connect(mapStateToProps)(Checkout);
+export default connect(mapStateToProps, mapDispatchToProps)(Checkout);
--- src/redux/actionCreators.js
+++ src/redux/actionCreators.js
@@ -19,3 +19,9 @@ export const updatePurchasable = () => {
     type: actionTypes.UPDATE_PURCHASABLE,
   };
 };
+
+export const reserIngredients = () => {
+  return {
+    type: actionTypes.RESET_INGREDIENTS,
+  }
+}

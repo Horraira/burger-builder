627d24478ee38eb96bcc22cd1b1964511a7e32cc
AbuSohan <sohan@inflexionpointbd.com>
2022-10-31 09:49:49 +0600

    Adding Validation - Formik

--- src/Components/Auth/Auth.js
+++ src/Components/Auth/Auth.js
@@ -10,9 +10,42 @@ export default class Auth extends Component {
           onSubmit={(values) => {
             console.log(values);
           }}
+          validate={(values) => {
+            const errors = {};
+
+            if (!values.email) {
+              errors.email = "Required";
+            } else if (
+              !/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(
+                values.email
+              )
+            ) {
+              errors.email = "Invalid email address";
+            }
+
+            if (!values.password) {
+              errors.password = "Required";
+            } else if (values.password.length < 4) {
+              errors.password = "Must be at least four Characters!";
+            }
+
+            if (!values.passwordConfirm) {
+              errors.passwordConfirm = "Required";
+            } else if (values.password != values.passwordConfirm) {
+              errors.passwordConfirm = "Password field doesn not match";
+            }
+            // console.log(errors);
+            return errors;
+          }}
         >
-          {({ values, handleChange, handleSubmit }) => (
-            <div>
+          {({ values, handleChange, handleSubmit, errors }) => (
+            <div
+              style={{
+                border: "1px grey solid",
+                padding: "15px",
+                borderRadius: "7px",
+              }}
+            >
               <form onSubmit={handleSubmit}>
                 <input
                   name="email"
@@ -20,7 +53,9 @@ export default class Auth extends Component {
                   className="form-control"
                   value={values.email}
                   onChange={handleChange}
-                />{" "}
+                />
+                <span style={{ color: "red" }}>{errors.email}</span>
+
                 <br />
                 <input
                   name="password"
@@ -28,7 +63,8 @@ export default class Auth extends Component {
                   className="form-control"
                   value={values.password}
                   onChange={handleChange}
-                />{" "}
+                />
+                <span style={{ color: "red" }}>{errors.password}</span>
                 <br />
                 <input
                   name="passwordConfirm"
@@ -36,7 +72,9 @@ export default class Auth extends Component {
                   className="form-control"
                   value={values.passwordConfirm}
                   onChange={handleChange}
-                />{" "}
+                />
+                <span style={{ color: "red" }}>{errors.passwordConfirm}</span>
+
                 <br />
                 <button type="submit" className="btn btn-success">
                   Sign Up
